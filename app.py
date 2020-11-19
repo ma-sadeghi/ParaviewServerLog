050cb8364c10dec7cb738a43a098caa8dfaa1274
Amin Sadeghi <amin.sadeghi@live.com>
2020-11-19 10:32:19 -0500

    Now, the webapp updates using Ajax instead of hard-refrehing every N secs, plus many more small changes

--- app.py
+++ app.py
@@ -29,16 +29,22 @@ def is_port_busy(number):
 
 def is_pvserver_available(service_name, port_number):
     status_service = is_service_running(service_name)
-    status_port = not is_port_busy(port_number) 
+    status_port = not is_port_busy(port_number)
     return status_service and status_port
 
 
 # Our so-called "databased"
 pvserver_instances = {
-    "pvserver1": 11111,
-    "pvserver2": 11112,
-    "pvserver3": 11113
+    0: {"name": "pvserver1", "port": 11111, "status": ""},
+    1: {"name": "pvserver2", "port": 11112, "status": ""},
+    2: {"name": "pvserver3", "port": 11113, "status": ""}
 }
+# Update availability based on realtime data
+status= {True: "available", False: "in-use"}
+for k, v in pvserver_instances.items():
+    is_available = is_pvserver_available(v["name"], v["port"])
+    v["status"] = status[is_available]
+
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -59,39 +65,28 @@ def home():
 
 @app.route('/api/v1/resources/pvserver', methods=['GET'])
 def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
+    r"""
+    Check if an ID was provided as part of the URL.
+    If ID is provided, assign it to a variable.
+    If no ID is provided, display an error in the browser.
+    """
     if 'port' in request.args:
         port = int(request.args['port'])
     else:
         return "Error: No port field provided. Please specify a port."
-    
+
     if 'name' in request.args:
         name = request.args['name']
     else:
         return "Error: No name field provided. Please specify a name."
 
-    # # Create an empty list for our results
-    # results = []
-
-    # # Loop through the data and match results that fit the requested ID.
-    # # IDs are unique, but other fields might return many results
-    # for book in books:
-    #     if book['id'] == id:
-    #         results.append(book)
-
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
-    # return jsonify(results)
     result = is_pvserver_available(name, port)
     return jsonify(result)
 
 
 @app.route('/api/v1/resources/pvserver/all', methods=['GET'])
 def api_all():
-    results = pvserver_instances
-    return jsonify(results)
+    return jsonify(pvserver_instances)
 
 
 if __name__ == "__main__":
--- static/app.js
+++ static/app.js
@@ -1,19 +1,32 @@
-$(function () {
-
-	var tableEl = $("#test")
-
+function updateTable() {
 	url = "/api/v1/resources/pvserver/all"
-	response = fetch(url);
-    var data = response.json();
+	response = $.getJSON(url, function(data){
+        populateTable(data);
+    });
+};
 
-	console.log(data)
+function populateTable(data){
+    // var d = new Date();
+    var r = new Array(), j = -1, counter_row = 1;
+    for (key in data){
+        color = (data[key]["status"] == "available") ? "green" : "red"
+        console.log(color)
+        r[++j] = '<tr>';
+        r[++j] = `<th class="d-none d-lg-block" scope="row">${counter_row++}</th>`;
+        r[++j] = `<td><code>${data[key]["name"]}</code></td>`;
+        r[++j] = `<td>${data[key]["port"]}</td>`;
+        // r[++j] = `<td>${d.getSeconds()}</td>`;
+        r[++j] = `<td><span class="icon ${data[key]["status"]}"></span></td>`;
+        r[++j] = `<td class="d-none d-lg-block" style="color: ${color}">${data[key]["status"]}</td>`;
+        r[++j] = '</tr>'
+    }
+    $('#tableRows').html(r.join(''));
+}
 
-});
+// Populate the table for the first time
+updateTable()
 
-// <tr>
-//   <th scope="row">{{ i+1 }}</th>
-//   <td><code>pvserver{{ i+1 }}</code></td>
-//   <td>{{ 11111+i }}</td>
-//   <td><span class="icon {{ statuses[i] }}"></span></td>
-//   <td style="color:{{ colors[i] }}">{{ statuses[i] }}</td>
-// </tr>
+// Refresh the table every 10 seconds
+window.setInterval(function(){
+    updateTable();
+}, 10000);
